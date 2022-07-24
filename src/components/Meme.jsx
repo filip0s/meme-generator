c15a921d73b8d94c6f741ab64e2995815945daed
Filip <dev.filip0s@protonmail.com>
2022-07-24 16:56:23 +0200

    `Form` component styling

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,23 @@
 const Meme = () => {
-  return (
-      <main className="meme">
-          <form className="meme--form" action="">
-              <input type="text"/>
-              <input type="text"/>
-              <button className="meme--form--button">
-                  Get a new meme image
-              </button>
-          </form>
-      </main>
-  )
+    return (
+        <main className="meme">
+            <form className="meme--form" action="">
+                <input
+                    className="meme--form--input"
+                    placeholder="Top text"
+                    type="text"
+                />
+                <input
+                    className="meme--form--input"
+                    placeholder="Bottom text"
+                    type="text"
+                />
+                <button className="meme--form--button">
+                    Get a new meme image
+                </button>
+            </form>
+        </main>
+    )
 }
 
 export default Meme;
