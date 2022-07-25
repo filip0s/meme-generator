6d3686ca13bf14a5d5f6e9a8a1c5ad31cd020dbe
Filip <dev.filip0s@protonmail.com>
2022-07-25 22:41:57 +0200

    Implemented `meme` state for the `Meme` component

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,24 @@
+import memesData from "../assets/data/memesData";
+import {useState} from "react";
 const Meme = () => {
+    const [meme, setMeme] = useState({
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+        topText: "",
+        bottomText: "",
+    });
+
+    const getRandomMemeImage = () => {
+        const memeArray = memesData.data.memes;
+        const randomMemeIdx = Math.floor(Math.random() * memeArray.length)
+        const {url} = memeArray[randomMemeIdx]
+        setMeme((prevState) => {
+            return {
+                ...prevState,
+                randomImage: url
+            }
+        })
+    }
+
     return (
         <main className="meme">
             <div className="meme--form" >
@@ -12,10 +32,15 @@ const Meme = () => {
                     placeholder="Bottom text"
                     type="text"
                 />
-                <button className="meme--form--button">
+                <button
+                    className="meme--form--button"
+                    onClick={getRandomMemeImage}
+                >
                     Get a new meme image
                 </button>
             </div>
+            <img className="meme--image" src={meme.randomImage} alt="Meme"/>
+
         </main>
     )
 }
